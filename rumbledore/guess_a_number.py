551fc8600ea5e9bd8dc70f8ae6005301f2e4dc32
rumbledore <moshedota@gmail.com>
2019-08-05 20:54:47 +0300

    rumbledore number guessing game

--- rumbledore/guess_a_number.py
+++ rumbledore/guess_a_number.py
@@ -2,28 +2,36 @@ import random
 
 is_playing = True
 score = 0
-
+number_of_guesses = 3
+min_num = 1
+max_num = 20
 
 while is_playing:
 
-    rnd_num = random.randint(1, 51)
-
+    rnd_num = random.randint(min_num, max_num + 1)
     print("---------- Welcome to Rumblesore's guess game ----------")
-    answer = int(input("Guess a number between 1-50: \n"))
 
-    if rnd_num == answer:
-        print("You guessed right!")
-        score += 1
+    while number_of_guesses > 0:
+        answer = int(input(f"Guess a number between {min_num}-{max_num}: \n"))
 
-    else:
-        print("Incorrect!")
-        if answer > rnd_num:
-            print("guess lower")
+        if rnd_num == answer:
+            print("You guessed right!")
+            score += 1
+
         else:
-            print("guess higher")
+            print("Incorrect!")
+            number_of_guesses -= 1
+            if not number_of_guesses > 0:
+                break
+
+            if answer > rnd_num:
+                print("guess lower")
+            else:
+                print("guess higher")
 
     answer = input("Wanna play again? (yes/no)\n")
     if answer == "yes":
+        number_of_guesses = 3
         continue
     else:
         is_playing = False
